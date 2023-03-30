e600157bc5f67d7bb1072108740698732cb33494
Harrison Shih <comedyinlyfe@gmail.com>
2023-03-30 18:52:13 -0400

    got rid of changes in app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,9 @@
 import EndpointComponent from './EndpointComponent/endpointComponent';
-import EndpointIcon from './EndpointTrayContainer/endpointIcon';
 
 export default function App() {
   return (
     <div>
       <EndpointComponent />
-      <EndpointIcon />
     </div>
   );
 }
