0b2246216953c059227709a062a08bdb406bfea4
Harrison Shih <comedyinlyfe@gmail.com>
2023-03-30 19:16:59 -0400

    added imports in app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,16 @@
 import EndpointComponent from './EndpointComponent/endpointComponent';
 import JSONFieldContainer from './JSONFieldContainer/jsonFieldContainer';
+import EndpointIcon from './EndpointTrayContainer/endpointIcon';
+import AllSchemasContainer from './SchemaView/containers/allSchemasContainer';
 
 export default function App() {
   return (
     <div>
       <EndpointComponent />
+      <EndpointIcon />
       <div className='bodyContainer'>
         <JSONFieldContainer />
+        <AllSchemasContainer />
       </div>
     </div>
   );
