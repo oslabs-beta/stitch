98f5eb0525d5d9c5f066abbd010c0c9d3756f689
Harrison Shih <comedyinlyfe@gmail.com>
2023-04-01 10:49:34 -0400

    put icon components on icontraycontainer

--- client/EndpointTrayContainer/iconTrayContainer.js
+++ client/EndpointTrayContainer/iconTrayContainer.js
@@ -1,1 +1,13 @@
 // tray containing all endpoints queried
+import EndpointIcon from './endpointIcon';
+
+
+export default function IconTrayContainer() {
+    return (
+      <div>
+        <EndpointIcon />
+        <EndpointIcon />
+        <EndpointIcon />
+      </div>
+    );
+  }
--- client/app.jsx
+++ client/app.jsx
@@ -1,13 +1,13 @@
 import EndpointComponent from './EndpointComponent/endpointComponent';
+import IconTrayContainer from './EndpointTrayContainer/iconTrayContainer';
 import JSONFieldContainer from './JSONFieldContainer/jsonFieldContainer';
-import EndpointIcon from './EndpointTrayContainer/endpointIcon';
 import AllSchemasContainer from './SchemaView/containers/allSchemasContainer';
 
 export default function App() {
   return (
     <div>
       <EndpointComponent />
-      <EndpointIcon />
+      <IconTrayContainer />
       <div className='bodyContainer'>
         <JSONFieldContainer />
         <AllSchemasContainer />
