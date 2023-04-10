7be7c6fd85bb7856d4bc5d7a30b60f6b4cc3e950
Nate Toll <nate.toll@gmail.com>
2023-04-10 14:51:10 -0400

    endpoint button styling

--- src/client/EndpointTrayContainer/endpointIcon.js
+++ src/client/EndpointTrayContainer/endpointIcon.js
@@ -4,15 +4,20 @@ import { useDispatch } from 'react-redux';
 import { updateActiveEndpoint } from '../store/slices/dataSlice';
 
 function EndpointIcon({ endpointURL }) {
-    const dispatch = useDispatch()
-    // will pass in image link via props to generate a diff image for each component
+  const dispatch = useDispatch();
+  // will pass in image link via props to generate a diff image for each component
 
-    return ( 
-        <div>
-             <button onClick={() => dispatch(updateActiveEndpoint(endpointURL))}>{endpointURL}</button>
-             {/* <button onClick={() => console.log('clicked button', endpointURL)}>{endpointURL}</button> */}
-        </div>
-    )
+  return (
+    <>
+      <button
+        className='bg-space-jet h-10 rounded ml-5 mr-5 mb-5 first:mt-5'
+        onClick={() => dispatch(updateActiveEndpoint(endpointURL))}
+      >
+        {endpointURL}
+      </button>
+      {/* <button onClick={() => console.log('clicked button', endpointURL)}>{endpointURL}</button> */}
+    </>
+  );
 }
 
 export default EndpointIcon;
