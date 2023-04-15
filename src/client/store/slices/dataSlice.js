175e382a18228e0e8ad95bb59d14a02b3399d48d
Keith Falcon <keith.falcon@gmail.com>
2023-04-15 11:05:36 -0400

    dataSlice endpointSlice and schemaSlice code cleanup

--- src/client/store/slices/dataSlice.js
+++ src/client/store/slices/dataSlice.js
@@ -7,8 +7,8 @@ const initialState = {
     responseBody: {},
   },
   githubUserSavedViews: {
-    views: []
-  }
+    views: [],
+  },
 };
 
 export const addDataCard = createAsyncThunk(
@@ -20,54 +20,38 @@ export const addDataCard = createAsyncThunk(
         'Content-type': 'application/json',
       },
     });
-    const data = await request.json()
-    // console.log('in reducer', data);
+    const data = await request.json();
     return data;
   }
 );
 
 export const saveGithubView = createAsyncThunk(
   'responseData/saveGithubView',
-  async ({viewName, id}, { getState, dispatch }) => {
-    // grab current state
-    const { responseData, schemaSlice} = getState();
+  async ({ viewName, id }, { getState }) => {
+    const { responseData, schemaSlice } = getState();
     const request = await fetch('/api/githubdata', {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify( { responseData, schemaSlice, viewName, id} ),
+      body: JSON.stringify({ responseData, schemaSlice, viewName, id }),
     });
-    const data = await request.json()
-    // console.log('in reducer', data);
+    const data = await request.json();
     return viewName;
   }
 );
 
 export const loadSavedGithubView = createAsyncThunk(
   'responseData/loadSavedGithubView',
-  async ({viewName, id}, { getState, dispatch }) => {
-    // grab current state
-    // const { responseData, schemaSlice} = getState();
-    console.log( { viewName, id } )
-    const { responseData, schemaSlice} = getState();
-    console.log('about to run get')
+  async ({ viewName, id }, { getState }) => {
     const request = await fetch('/api/getview', {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': 'application/json',
       },
-      body: JSON.stringify( { viewName, id } ),
+      body: JSON.stringify({ viewName, id }),
     });
-    // console.log('done with request', request);
-    const data = await request.json()
-    // console.log('data response', data);
-    // console.log('in reducer', data);
-    // console.log(state.responseData)
-    // console.log('schemaSlice', schemaSlice)
-    // state.responseData = data.responseData
-    // schemaSlice.schemaFields = data.schemaSlice.schemaFields
-    // console.log(state.list)
+    const data = await request.json();
     return data;
   }
 );
@@ -77,21 +61,14 @@ export const dataSlice = createSlice({
   initialState,
   reducers: {
     updateActiveEndpoint: (state, action) => {
-      console.log('in update active endpoint reducer');
-      // console.log({action})
-
       state.activeEndpoint = {
         url: action.payload,
         responseBody: state.endpointData[action.payload],
       };
     },
     storeGithubUserView: (state, action) => {
-      console.log('in storeGithubUserView reducer');
-      console.log({action})
-      // console.log({action.payload})
-
       state.githubUserSavedViews = {
-        views: [...state.githubUserSavedViews.views, action.payload]
+        views: [...state.githubUserSavedViews.views, action.payload],
       };
     },
   },
@@ -107,28 +84,20 @@ export const dataSlice = createSlice({
     // Save View Promise Resolve Handler
     builder.addCase(saveGithubView.fulfilled, (state, action) => {
       state.githubUserSavedViews = {
-        views: [...state.githubUserSavedViews.views, action.meta.arg.viewName]
+        views: [...state.githubUserSavedViews.views, action.meta.arg.viewName],
       };
     });
     // Update state with saved view data
-    // Save View Promise Resolve Handler
-    // NEED TO PULL IN ALL OF STATE AND UPDATE IT HERE:  Keith 4/14 8:11pm
     builder.addCase(loadSavedGithubView.fulfilled, (state, action) => {
-      // state.githubUserSavedViews = {
-      //   views: [...state.githubUserSavedViews.views, action.meta.arg.viewName]
-      // };
-      // console.log(action.meta.arg)
-      // console.log('in builder', console.log(action.meta.arg));
-      // console.log(action.payload.responseData.endpointData)
-      // const { schemaSlice } = getState();
       state.endpointData = action.payload.responseData.endpointData;
       state.activeEndpoint = action.payload.responseData.activeEndpoint;
-      // console.log('schemaSlice', state.schemaSlice)
-      // console.log('schemaSlice', state.typeName)
-      // console.log('schemaSlice', state.schemaFields)
     });
-  }
-})
-    
-export const { updateActiveEndpoint, setActiveUserGithubInfo, storeGithubUserView } = dataSlice.actions
+  },
+});
+
+export const {
+  updateActiveEndpoint,
+  setActiveUserGithubInfo,
+  storeGithubUserView,
+} = dataSlice.actions;
 export default dataSlice.reducer;
--- src/client/store/slices/schemaSlice.js
+++ src/client/store/slices/schemaSlice.js
@@ -1,39 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
-import utilityFunctions from "../../../utilities/utilities";
+import { createSlice } from '@reduxjs/toolkit';
+import utilityFunctions from '../../../utilities/utilities';
 const { scalarParser, snakeToCamel } = utilityFunctions;
 import { loadSavedGithubView } from '../slices/dataSlice';
 
 const initialState = {
   typeName: {
-    name: "",
+    name: '',
   },
-  schemaFields: {
-    // type: '',
-    // fields: {},
-  },
+  schemaFields: {},
   activeSchema: {
-    name: "",
+    name: '',
   },
   graphQL: {},
 };
 
 export const schemaSlice = createSlice({
-  name: "schemaSlice",
+  name: 'schemaSlice',
   initialState,
   reducers: {
     setTypeName: (state, action) => {
-      console.log('This be Typename: ', action.payload);
       state.typeName = action.payload;
-      console.log('This be Typename in State: ', state.typeName);
     },
     addSchemaField: (state, action) => {
-      // come back to this - determine type of val
       const objectValue = action.payload.objectValue;
       state.schemaFields[snakeToCamel(action.payload.objectKey)] =
         scalarParser(objectValue);
     },
     toggleRequired: (state, action) => {
-      // if not an Array, we only have 2 options to cycle between (required or Not required)
       if (!state.schemaFields[action.payload].isArray) {
         if (state.schemaFields[action.payload].requiredOption === 0) {
           state.schemaFields[action.payload].requiredOption = 1;
@@ -50,16 +43,9 @@ export const schemaSlice = createSlice({
           state.schemaFields[action.payload].requiredOption = 1;
         }
         state.schemaFields[action.payload].requiredOption++;
-        // else {
-        //   state.schemaFields[action.payload].requiredOption++;
-        // }
       }
     },
     deleteSchemaField: (state, action) => {
-      console.log(
-        "in delete schema field reducer, objectKey:",
-        action.payload.objectKey
-      );
       delete state.schemaFields[action.payload.objectKey];
     },
     isArrayChange: (state, action) => {
@@ -71,21 +57,10 @@ export const schemaSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    // NEED TO PULL IN ALL OF STATE AND UPDATE IT HERE:  Keith 4/14 8:11pm
     builder.addCase(loadSavedGithubView.fulfilled, (state, action) => {
-      // state.githubUserSavedViews = {
-      //   views: [...state.githubUserSavedViews.views, action.meta.arg.viewName]
-      // };
-      // console.log(action.meta.arg)
-      // console.log('in builder', console.log(action.meta.arg));
-      // console.log(action.payload.responseData.endpointData)
-      // const { schemaSlice } = getState();
-      console.log('in schemaSlice but found dataSlice reducer')
-      state.schemaFields = action.payload.schemaSlice.schemaFields
-      // state.endpointData = action.payload.responseData.endpointData;
-      // state.activeEndpoint = action.payload.responseData.activeEndpoint;
+      state.schemaFields = action.payload.schemaSlice.schemaFields;
     });
-  }
+  },
 });
 
 export const {
